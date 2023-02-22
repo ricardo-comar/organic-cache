3a089c8219180042658971440482fc754460049a
Ricardo Henrique Comar <ricardo.comar@gmail.com>
2023-02-22 19:17:22 -0300

    localstach 1.4.0

--- elasticache.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_elasticache_cluster" "quotation-cache" {
-  cluster_id           = "quotation-cache-cluster"
-  engine               = "redis"
-  node_type            = "cache.t2.medium"
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
-}
